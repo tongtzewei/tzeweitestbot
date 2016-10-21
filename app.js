921e1a7608aefe076c9a97ff1ef1e19761f1975f
Tong Tze Wei <tongtzewei@outlook.com>
2016-10-21 18:23:19 +0800

    new files: app.js, package.json, .vscode\launch.json

---
+++ app.js
@@ -0,0 +1,21 @@
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: 'f03f0907-1788-4c5f-b6d7-a2ddfcb1288b', appPassword: 'ueuFNyKzQzgvz0vRcxfRRna' }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
